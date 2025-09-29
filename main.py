ad0d49b8ea6838dfec58d0a210e7846bf9394d37
pablokreick <pablokreick@gmail.com>
2025-09-29 00:14:15 -0300

    fix las bolas ya no aparecen pegadas

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
     sound_fail = pygame.mixer.Sound(join("sounds", "fail.mp3"))
 
     pygame.mixer.music.load(join("sounds", "music.mp3"))
-    # pygame.mixer.music.play(-1)
+    pygame.mixer.music.play(-1)
     pygame.mixer.music.set_volume(0.05)
 
     # Grupos de sprites
@@ -123,13 +123,13 @@ def main():
                         sound_wrong.play()
                         player.lose_life()
                         player.add_score(-1)
-                        # if player.has_no_lives():
-                        #     sound_fail.play()
-                        #     play = f.final_menu(display, False)
-                        #     if play:
-                        #         in_game = False
-                        #         game.restart(ball_sprites)
-                        #         player.restart()
+                        if player.has_no_lives():
+                            sound_fail.play()
+                            play = f.final_menu(display, False)
+                            if play:
+                                in_game = False
+                                game.restart(ball_sprites)
+                                player.restart()
 
             # ---------------------------------------------------------------------------- #
             #                                DISPLAY SPRITES                               #
